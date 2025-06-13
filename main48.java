4f7c6df190f9a8f123b10af03198f7cff9bdaf43
JyothiThammireddy123 <jyothithammireddy03@gmail.com>
2025-06-13 16:03:24 +0530

    Add files via upload

---
+++ main48.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		for(int i=0; i=a<length(); i++)
+		{
+			if(char.is digit(ch)==false) {
+				System.out.println("not digits");
+				System.exit(0);
+			}
+		}
+		System.out.println("digits");
+	}
+}
